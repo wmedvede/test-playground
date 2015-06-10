02eca2e07412bf7f95257f7055498ec3017c97b3
Max Barkley <mbarkley@redhat.com>
2015-06-10 13:53:02 -0400

    Load all list views onto default page.

--- users/src/main/java/demo/client/local/MainPage.java
+++ users/src/main/java/demo/client/local/MainPage.java
@@ -1,27 +1,47 @@
 package demo.client.local;
 
+import java.util.Collection;
+
+import javax.annotation.PostConstruct;
 import javax.inject.Inject;
 
+import org.jboss.errai.ioc.client.container.IOCBeanDef;
+import org.jboss.errai.ioc.client.container.SyncBeanManager;
 import org.jboss.errai.ui.nav.client.local.DefaultPage;
 import org.jboss.errai.ui.nav.client.local.Page;
-import org.jboss.errai.ui.nav.client.local.PageShowing;
 import org.jboss.errai.ui.shared.api.annotations.DataField;
 import org.jboss.errai.ui.shared.api.annotations.Templated;
+import org.livespark.formmodeler.rendering.client.view.ListView;
 
-import com.github.gwtbootstrap.client.ui.TextBox;
 import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.VerticalPanel;
 
 @Page( role = DefaultPage.class )
 @Templated
+@SuppressWarnings("rawtypes")
 public class MainPage extends Composite {
 
     @Inject
     @DataField
-    private TextBox test;
+    private VerticalPanel container;
 
-    @PageShowing
-    private void showing() {
-        test.setText( "Proof" );
+    @Inject
+    private SyncBeanManager beanManager;
+
+    @PostConstruct
+    private void init() {
+        final Collection<IOCBeanDef<ListView>> listViewBeans = beanManager.lookupBeans( ListView.class );
+
+        for ( IOCBeanDef<ListView> listViewBean : listViewBeans ) {
+            if ( isInstantiable( listViewBean ) ) {
+                final ListView instance = listViewBean.getInstance();
+                container.add( instance );
+            }
+        }
+    }
+
+    private boolean isInstantiable( IOCBeanDef<ListView> listViewBean ) {
+        return listViewBean.isConcrete() && listViewBean.isActivated();
     }
 
 }
