65ad593687b95380182d2c45606aac573e862529
Max Barkley <mbarkley@redhat.com>
2015-06-12 16:13:34 -0400

    Load data from postconstruct of main page.

--- users/src/main/java/demo/client/local/builtin/MainPage.java
+++ users/src/main/java/demo/client/local/builtin/MainPage.java
@@ -2,13 +2,14 @@ package demo.client.local.builtin;
 
 import java.util.Collection;
 
+import javax.annotation.PostConstruct;
+import javax.enterprise.context.Dependent;
 import javax.inject.Inject;
 
 import org.jboss.errai.ioc.client.container.IOCBeanDef;
 import org.jboss.errai.ioc.client.container.SyncBeanManager;
 import org.jboss.errai.ui.nav.client.local.DefaultPage;
 import org.jboss.errai.ui.nav.client.local.Page;
-import org.jboss.errai.ui.nav.client.local.PageShowing;
 import org.jboss.errai.ui.shared.api.annotations.DataField;
 import org.jboss.errai.ui.shared.api.annotations.Templated;
 import org.livespark.formmodeler.rendering.client.view.ListView;
@@ -19,6 +20,7 @@ import com.google.gwt.user.client.ui.VerticalPanel;
 
 @Page( role = DefaultPage.class )
 @Templated
+@Dependent
 @SuppressWarnings("rawtypes")
 public class MainPage extends Composite {
 
@@ -32,7 +34,7 @@ public class MainPage extends Composite {
     @Inject
     private SyncBeanManager beanManager;
 
-    @PageShowing
+    @PostConstruct
     private void showing() {
         logger.debug( "Initializing MainPage" );
         final Collection<IOCBeanDef<ListView>> listViewBeans = beanManager.lookupBeans( ListView.class );
@@ -43,6 +45,7 @@ public class MainPage extends Composite {
             if ( isInstantiable( listViewBean ) ) {
                 logger.debug( "Instantiating " + listViewBean.getBeanClass().getName() );
                 final ListView instance = listViewBean.getInstance();
+                instance.init();
                 container.add( instance );
             }
         }
